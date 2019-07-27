3f0c15a1ca1106fab77f47814740e351f96fa7b1
naaman94 <naaman.monther@gmail.com>
2019-07-28 00:54:53 +0300

    finish the desgin and add hint key

--- jquery.js
+++ jquery.js
@@ -21,8 +21,12 @@ var arrColor = ["question-mark.png", "https://dummyimage.com/200x200/d62462/4196
     "https://dummyimage.com/200x200/507cc0/4196e5",
     "https://dummyimage.com/200x200/e4e4e4/4196e5",
     "https://dummyimage.com/200x200/e4e4e4/4196e5",
-    "https://dummyimage.com/200x200/000000/4196e5",
-    "https://dummyimage.com/200x200/000000/4196e5"
+    "https://dummyimage.com/200x200/ffff00/4196e5",
+    "https://dummyimage.com/200x200/ffff00/4196e5",
+    "https://dummyimage.com/200x200/ff00ff/4196e5",
+    "https://dummyimage.com/200x200/ff00ff/4196e5",
+    "https://dummyimage.com/200x200/ff9100/4196e5",
+    "https://dummyimage.com/200x200/ff9100/4196e5"
 
 ];
 var color = {}
@@ -34,7 +38,12 @@ var time = 5000;
 
 selectNumOfBlock(ele)
 hiden(".allImg", time)
-
+// for (let i = 1; i <= 20; i++) {
+//     $(document).ready(function () {
+//     $('#master').html("<div><img alt=\"Responsive image\"></div>")
+//     })
+//     console.log(i)
+// }here am trying to make it creat html code in for loop but its not work 
 $("#edit").click(function () {
     if ($("input[name='options']:checked").attr("id") == "option1") { time = 10000 }
     if ($("input[name='options']:checked").attr("id") == "option2") { time = 5000 }
@@ -56,9 +65,9 @@ $("#edit").click(function () {
         hiden(".allImg", time)
         chk();
     }
-    if ($("input[name='numOfblocks1']:checked").val() == "20") {
+    if ($("input[name='numOfblocks1']:checked").val() == "24") {
         col = 2;
-        ele = 20;
+        ele = 24;
         selectNumOfBlock(ele);
         show();
         hiden(".allImg", time)
@@ -73,7 +82,7 @@ function chk() {
     $(document).ready(function () {
         for (let i = 1; i <= ele; i++) {
             $('#master').on('click', '.img' + i, function () {
-                
+
                 if (checkColor.check1st == "" && ($("#img" + i).attr("srcset") == arrColor[0])) {
                     checkColor.index1st = "#img" + i
                     checkColor.check1st = "img" + odd(i)
@@ -116,10 +125,12 @@ function odd(i) {
     else
         return i
 }
+
 function show() {
-    for (let i = 1; i <= 20; i++) {
+    for (let i = 1; i <= 24; i++) {
 
         $(document).ready(function () {
+            $("#master div:nth-child(" + i + ")").removeClass()
             $("#master div:nth-child(" + i + ") img ").attr("srcset", arrColor[i]);
             $("#master div:nth-child(" + i + ")").addClass("col-" + col + " img" + i + " order-" + rand())
             $("#master div:nth-child(" + i + ") img ").addClass("img-thumbnail allImg").attr("id", "img" + i);
@@ -131,19 +142,49 @@ function show() {
 }
 
 function selectNumOfBlock(ele) {
-    for (let i = 1; i <= 20; i++) {
+    for (let i = 1; i <= 24; i++) {
         $("#master div:nth-child(" + i + ")").attr("hidden", false);
     }
-    for (let i = ele + 1; i <= 20; i++) {
+    for (let i = ele + 1; i <= 24; i++) {
         $("#master div:nth-child(" + i + ")").attr("hidden", true);
 
     }
 }
 
-function rand() { return Math.round(Math.random() * 20); }
+function rand() { return Math.round(Math.random() * 24); }
 
 function youWin() {
-
-
-
+    $('#congratulations').modal('show');
 }
+
+$('#playAgain').click(function () {
+    location.reload();
+});
+
+
+$(document).ready(function () {
+    $("#hints").click(function () {
+        $("#hints").attr("disabled", true)
+        $('#gameset').modal('hide');
+        for (let i = 1; i <= ele; i++) {
+            if ($("#img" + i).attr("srcset") == arrColor[0]) {
+                if (i % 2 == 0) {
+                    $("#img" + i).attr("srcset", color["img" + i])
+                    $("#img" + (i - 1)).attr("srcset", color["img" + (i - 1)])
+                }
+                else {
+                    $("#img" + i).attr("srcset", color["img" + i])
+                    $("#img" + (i + 1)).attr("srcset", color["img" + (i + 1)])
+                }
+                count++
+                if (count == ele / 2) {
+                    youWin();
+                }
+                break;
+
+            }
+        }
+    })
+})
+
+
